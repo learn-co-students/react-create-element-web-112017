d77a8e38c8a50195456613460d91e22d14753ac8
ddbrennan <ddbren@gmail.com>
2018-01-28 13:35:35 -0500

    added elements to dom with react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My first react code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!')
+
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
+      ]
+    )
+  );
+
+const container = React.createElement('div', {}, [title, paragraph, list]);
+
+ReactDOM.render(
+  container,
+  document.getElementById('global')
+);
